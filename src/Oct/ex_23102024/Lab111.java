c86aaec0d610c4e29994d11be1cdf0122bfd3b9e
Kavita-24 <er.kavitasi@gmail.com>
2024-10-30 07:31:17 +0530

    22 Oct 2024 All Commited

---
+++ src/Oct/ex_23102024/Lab111.java
@@ -0,0 +1,12 @@
+package Oct.ex_23102024;
+
+public class Lab111 {
+    public static void main(String[] args) {
+        greet();
+        greet();
+    }
+    static void greet()
+    {
+        System.out.println("Hello, how are you?");
+    }
+}
---
+++ src/Oct/ex_23102024/Lab112.java
@@ -0,0 +1,8 @@
+package Oct.ex_23102024;
+
+public class Lab112 {
+    public static void main(String[] args) {
+        int result=Math.max(13,41);//Internally uses ternary operator
+        System.out.println(result);
+    }
+}
