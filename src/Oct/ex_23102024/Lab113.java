b5218785631443b225b002696146ee1c403c4152
Kavita-24 <er.kavitasi@gmail.com>
2024-11-05 11:29:54 +0530

    23 Oct 2024 All Commited

---
+++ src/Oct/ex_23102024/Lab113.java
@@ -0,0 +1,16 @@
+package Oct.ex_23102024;
+
+public class Lab113 {
+    public static void main(String[] args) {
+        //Functions
+        //Block of code which we can reuse
+
+        //Built In - Math.max,min .... String.. Time,Date
+        //User defined
+
+        //Two components of the functions
+        //1. Define / Declare
+        //2. Call the function
+    }
+
+}
