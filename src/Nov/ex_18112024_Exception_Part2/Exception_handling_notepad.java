0194a156d9b9b0866311c9e6cf3236a58802dbe1
Kavita-24 <er.kavitasi@gmail.com>
2024-11-18 19:36:21 +0530

    18 Nov 2024

---
+++ src/Nov/ex_18112024_Exception_Part2/Exception_handling_notepad.java
@@ -0,0 +1,40 @@
+package Nov.ex_18112024_Exception_Part2;
+public class Exception_handling_notepad
+{
+	public static void main(String[] args)
+	{
+		String a=null;
+		try
+		{
+			a=args[0];
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		int b=0;
+		try
+		{
+			b=Integer.parseInt(a);
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		int c=0;
+		try
+		{
+			c=b/0;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+		finally
+		{
+			System.out.println("End of Program");
+		}
+		
+	}
+}
